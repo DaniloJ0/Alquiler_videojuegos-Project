0287f1fd318b801c9ee9fde8fa2d25b175b06c52
Danilo Jimenez <rangeljd@uninorte.edu.co>
2023-05-27 10:17:09 -0500

    Add Models

---
+++ DBContext/AppDbContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Alquiler_videojuegos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alquiler_videojuegos.DBContext;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Client> Clients { get; set; }
+
+    public virtual DbSet<Game> Games { get; set; }
+
+    public virtual DbSet<Rent> Rents { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Rent>(entity =>
+        {
+            entity.HasOne(d => d.IdGameNavigation).WithMany(p => p.Rents)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_rent_game");
+
+            entity.HasOne(d => d.IdUserNavigation).WithMany(p => p.Rents)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_rent_user");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasOne(d => d.IdClientNavigation).WithMany(p => p.Users)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_user_client");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alquiler_videojuegos.Models;
+
+[Table("user")]
+public partial class User
+{
+    [Key]
+    [Column("id_user")]
+    public int IdUser { get; set; }
+
+    [Column("id_client")]
+    public int IdClient { get; set; }
+
+    [Column("admin")]
+    public bool Admin { get; set; }
+
+    [Column("password")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string Password { get; set; } = null!;
+
+    [ForeignKey("IdClient")]
+    [InverseProperty("Users")]
+    public virtual Client IdClientNavigation { get; set; } = null!;
+
+    [InverseProperty("IdUserNavigation")]
+    public virtual ICollection<Rent> Rents { get; set; } = new List<Rent>();
+}
